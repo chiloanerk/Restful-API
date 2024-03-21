14e3ab0d71c44ec9eb51eca8ab13415654f70af5
chiloanerk <chiloanerk@gmail.com>
2024-03-21 22:52:28 +0200

    For orders table we now have get, get by id, post, patch and delete methods

--- api/routes/products.js
+++ api/routes/products.js
@@ -6,10 +6,23 @@ const Product = require('../models/product');
 
 router.get("/", (req, res, next) => {
   Product.find()
+      .select('name price _id') // All the items i want to select
+      // .select('-__v')    alternative way, if i want to exclude i can use this
       .exec()
       .then( docs => {
-          console.log(docs);
-          res.status(200).json(docs);
+          const response = {
+              count: docs.length,
+              products: docs.map(({ _id, name, price }) => {
+                  return {
+                      name, price, _id,
+                      request: {
+                          type: 'GET',
+                          url: 'http://localhost:3000/products/' + _id
+                      }
+                  }
+              })
+          };
+          res.status(200).json(response);
       })
       .catch(err => {
           console.log(err);
@@ -27,11 +40,22 @@ router.post('/', (req, res, next) => {
         name: req.body.name,
         price: req.body.price
     });
-    product.save().then(result => {
-        console.log(result);
-        res.status(201).json({
-            message: 'Handling POST requests to /products',
-            createdProduct: result
+    product
+        .save()
+        .then(result => {
+            console.log(result);
+            res.status(201).json({
+            message: 'Created product successfully',
+            createdProduct: {
+                name: result.name,
+                price: result.price,
+                _id: result._id,
+                request: {
+                    type: 'GET',
+                    url: 'http://localhost:3000/products/' + result._id
+                }
+
+            }
         });
     })
         .catch(err => {
@@ -45,11 +69,17 @@ router.post('/', (req, res, next) => {
 router.get('/:productId', (req, res, next) => {
    const id = req.params.productId;
    Product.findById(id)
+       .select('-__v')
        .exec()
-       .then(docs => {
-           console.log(docs);
-           if (docs) {
-               res.status(200).json(docs);
+       .then(doc => {
+           if (doc) {
+               res.status(200).json({
+                   product: doc,
+                   request: {
+                       type: 'GET',
+                       url: 'http://localhost:3000/products/' + doc._id
+                   }
+               });
            } else {
                res.status(404).json({
                    message: 'No valid entry found for provided ID'
@@ -71,8 +101,13 @@ router.patch('/:productId', (req, res, next) => {
     Product.updateOne({_id: id}, {$set: updateOps})
         .exec()
         .then(result => {
-            console.log(result);
-            res.status(200).json(result);
+            res.status(200).json({
+                message: 'Product updated',
+                request: {
+                    type: 'GET',
+                    url: 'http://localhost:3000/products/' + id
+                }
+            });
         })
         .catch(err => {
             console.error(err);
@@ -87,7 +122,14 @@ router.delete('/:productId', (req, res, next) => {
     Product.deleteOne({_id: id})    // used to be remove function but is no longer used in mongodb
         .exec()
         .then(result => {
-            res.status(200).json(result);
+            res.status(200).json({
+                message: 'Product deleted',
+                request: {
+                    type: 'POST',
+                    url: 'http://localhost:3000/products/',
+                    body: { name: 'String', price: 'Number'}
+                }
+            });
         })
         .catch(err => {
             console.log(err);
