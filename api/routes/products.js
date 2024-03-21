baf1721b1ddb003520ed996bbd340d66338f4638
chiloanerk <chiloanerk@gmail.com>
2024-03-21 16:55:10 +0200

    implemented get, get by id, post, patch and delete methods

--- api/routes/products.js
+++ api/routes/products.js
@@ -5,9 +5,20 @@ const mongoose = require('mongoose');
 const Product = require('../models/product');
 
 router.get("/", (req, res, next) => {
-  res.status(200).json({
-    message: "Handling GET requests to /products",
-  });
+  Product.find()
+      .exec()
+      .then( docs => {
+          console.log(docs);
+          res.status(200).json(docs);
+      })
+      .catch(err => {
+          console.log(err);
+          // Sometimes it doesn't want me to add this response here for some reason,
+          // if there is an error i just comment it out
+          res.status(500).json({
+              error: err
+          });
+      });
 });
 
 router.post('/', (req, res, next) => {
@@ -18,21 +29,32 @@ router.post('/', (req, res, next) => {
     });
     product.save().then(result => {
         console.log(result);
+        res.status(201).json({
+            message: 'Handling POST requests to /products',
+            createdProduct: result
+        });
     })
-        .catch(err => console.log(err));
-    res.status(201).json({
-        message: 'Handling POST requests to /products',
-        createdProduct: product
-    });
+        .catch(err => {
+            console.log(err);
+                res.status(500).json({
+                    error: err
+                })
+        });
 });
 
 router.get('/:productId', (req, res, next) => {
    const id = req.params.productId;
    Product.findById(id)
        .exec()
-       .then(doc => {
-           console.log(doc);
-           res.status(200).json(doc);
+       .then(docs => {
+           console.log(docs);
+           if (docs) {
+               res.status(200).json(docs);
+           } else {
+               res.status(404).json({
+                   message: 'No valid entry found for provided ID'
+               });
+           }
        })
        .catch(err => {
            console.log(err);
@@ -41,15 +63,38 @@ router.get('/:productId', (req, res, next) => {
 });
 
 router.patch('/:productId', (req, res, next) => {
-    res.status(200).json({
-       message: 'Updated product!',
-    });
+    const id = req.params.productId;
+    const updateOps = {};
+    for (const ops of req.body) {
+        updateOps[ops.propName] = ops.value;
+    }
+    Product.updateOne({_id: id}, {$set: updateOps})
+        .exec()
+        .then(result => {
+            console.log(result);
+            res.status(200).json(result);
+        })
+        .catch(err => {
+            console.error(err);
+            res.status(500).json({
+                error: err
+            });
+        });
 });
 
 router.delete('/:productId', (req, res, next) => {
-    res.status(200).json({
-        message: 'Deleted product!',
-    });
+    const id = req.params.productId;
+    Product.deleteOne({_id: id})    // used to be remove function but is no longer used in mongodb
+        .exec()
+        .then(result => {
+            res.status(200).json(result);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
 });
 
 
